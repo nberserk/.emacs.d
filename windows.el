dc2b8bf7dcf759ac1301bfc87b7af18bf244843c
Darren Ha <nberserk@gmail.com>
2012-02-29 18:25:58 -0800

    bug fix on windows.el

--- windows.el
+++ windows.el
@@ -5,3 +5,5 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#102e4e" :foreground "#eeeeee" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Consolas")))))
+
+(provide 'windows)

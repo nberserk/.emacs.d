3a15e8997f185d29af7d6b39c9adb5c2951899c3
Darren Ha <darren.ha@samsung.com>
2014-07-25 15:27:44 +0900

    add eshell-here

--- init.el
+++ init.el
@@ -514,6 +514,40 @@ in current buffer."
   (interactive)
   (org-export-as-html nil nil nil nil t nil))
 
+;; --------------------------------------------------------------------------
+;; eshell
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the current buffer's file."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (name (car (last (split-string parent "/" t)))))
+    (split-window-vertically)
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(defun delete-single-window (&optional window)
+  "Remove WINDOW from the display.  Default is `selected-window'.
+If WINDOW is the only one in its frame, then `delete-frame' too."
+  (interactive)
+  (save-current-buffer
+    (setq window (or window (selected-window)))
+    (select-window window)
+    (kill-buffer)
+    (if (one-window-p t)
+        (delete-frame)
+        (delete-window (selected-window)))))
+
+(defun eshell/x ()
+  (delete-single-window))
+;; end of eshell
+;; --------------------------------------------------------------------------
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.

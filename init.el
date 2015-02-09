e5a0f2fe1f211cd14a1888dc13b624eca2e7343f
Darren Ha <nberserk@gmail.com>
2015-02-09 18:33:14 +0900

    update for snippet

--- init.el
+++ init.el
@@ -153,6 +153,7 @@
 ; c/cpp
 (require 'cc-mode)
 (setq-default tab-width 4
+
               c-basic-offset 4
               indent-tabs-mode nil)
 (add-hook 'c-mode-common-hook
@@ -565,9 +566,43 @@ If WINDOW is the only one in its frame, then `delete-frame' too."
      '(custom-enabled-themes (quote (wombat)))
    ) 
  )
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(font-lock-string-face ((t (:foreground "color-34")))))
+
+;; delete text not kill into kill-ring
+(defun my-delete-word (arg)
+  "Delete characters forward until encountering the end of a word.
+With argument, do this that many times.
+This command does not push erased text to kill-ring."
+  (interactive "p")
+  (delete-region (point) (progn (forward-word arg) (point))))
+
+(defun my-backward-delete-word (arg)
+  "Delete characters backward until encountering the beginning of a word.
+With argument, do this that many times.
+This command does not push erased text to kill-ring."
+  (interactive "p")
+  (my-delete-word (- arg)))
+
+(defun my-delete-line ()
+  "Delete text from current position to end of line char."
+  (interactive)
+  (delete-region
+   (point)
+   (save-excursion (move-end-of-line 1) (point)))
+  (delete-char 1)
+)
+
+(defun my-delete-line-backward ()
+  "Delete text between the beginning of the line to the cursor position."
+  (interactive)
+  (let (x1 x2)
+    (setq x1 (point))
+    (move-beginning-of-line 1)
+    (setq x2 (point))
+    (delete-region x1 x2)))
+
+; Here's the code to bind them with emacs's default shortcut keys:
+
+(global-set-key (kbd "M-d") 'my-delete-word)
+(global-set-key (kbd "<M-backspace>") 'my-backward-delete-word)
+;; (global-set-key (kbd "C-k") 'my-delete-line)
+;; (global-set-key (kbd "C-S-k") 'my-delete-line-backward)
